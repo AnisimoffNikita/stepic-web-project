69cf229a52a43fdd0cdd4092ddb555642d642734
Nikita <anisimoff.nikita@gmail.com>
2018-02-06 00:20:45 +0300

    another fix

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 def wsgi_application(environ, start_response):
     qs = environ['QUERY_STRING']
-    response_body = [bytes(i + '\n', 'ascii') for i in qs.split('&')]
+    response_body = [i + '\n' for i in qs.split('&')]
+    print response_body
+     
+    l = 0
+    for x in response_body:
+        l += len(x)
 
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'text/plain'),
-        ('Content-Length', str(len(response_body)))
+        ('Content-Length', str(l))
     ]
     start_response(status, response_headers)
     return response_body
